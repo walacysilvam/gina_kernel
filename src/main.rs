8e9fde985d2c5428b42b1f2c497218cadfcf532f
walacysilva <walacysm@gmail.com>
2022-10-30 21:42:51 -0300

    kernel inicializando e mostrando mensagem basica.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,28 @@
-//
-//      Gina OS 
-//  versao 0.1 - teste
+/*
+              Gina Kernel 
+    versao 0.1 - teste/experimental
+*/
 
-#![no_std]  //retira qualquer lib.
-#![no_main] //retira qualquer ponto de entrada.
+#![no_std]
+#![no_main]
 
-// usado para criar a panic-handler
 use core::panic::PanicInfo;
 
-// funcao panic handler para retornar erros,
-// nao existe panic handler nem qualquer outra funcao aqui
-// pre definidas. Compilacao "pura".
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+mod vga_buffer;
 
-// funcao de entrada substituindo a inicalizacao C padrao.
-// a funcao de entrada DEVE se chamar _start
-
-static HELLO: &[u8] = b"Gina's Alive!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    //convertendo o buffer em um ponteiro bruto 
-    let vga_buffer = 0x8000 as *mut u8;
-    
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize *2 + 1) = 0xb;
-        }
-    }
+    println!("==================================================");
+    println!("=        kernel running on version => ({})      =", 0.1);
+    println!("==================================================");
+    println!("=> Gina kernel iniciado :: Aguardando console...");
 
     loop {}
 }
 
+// Retorna o panic handler
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
